82704a1ff7d3ba2fae3f3761a3a6ba71d08d89b7
Pavithra220607 <pavithra.knpv@gmail.com>
2025-05-18 04:12:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],  // ← Tells Tailwind where to find your React files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
